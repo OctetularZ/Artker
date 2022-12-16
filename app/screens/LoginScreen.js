45996a83cab1a81026a093323550c97c4db2493d
OctetularZ <taiabayomi15@gmail.com>
2022-12-16 21:07:05 +0000

    Fixed errors caused by prettify

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,29 +1,14 @@
-import {
-  SafeAreaView,
-  useWindowDimensions,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  Image
-} from 'react-native'
-import React, {
-  useState,
-  useEffect
-} from 'react'
+import { SafeAreaView, useWindowDimensions, StyleSheet, Text, TextInput, View, Image } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import Logo from '../assets/app_logo_test.png'
 
 import Screen from '../components/Screen'
 import colours from '../config/colours'
 import CustomInput from '../components/CustomInput'
 import CustomButton1 from '../components/CustomButton1'
-import {
-  ScrollView
-} from 'react-native-gesture-handler'
+import { ScrollView } from 'react-native-gesture-handler'
 import SocialSignInButtons from '../components/SocialSignInButtons'
-import {
-  useNavigation
-} from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native'
 import * as SQLite from 'expo-sqlite'
 
 
@@ -35,34 +20,34 @@ export default function LoginScreen() {
 
   useEffect(() => {
     db.transaction(tx => {
-      tx.executeSql('CREATE TABLE IF NOT EXISTS Artker (id INTEGER PRIMARY KEY AUTOINCREMENT, Username TEXT, Email TEXT, Password TEXT )')
+      tx.executeSql('CREATE TABLE IF NOT EXISTS Artker (id INTEGER PRIMARY KEY AUTOINCREMENT, Username TEXT, Email TEXT, Password TEXT)')
     });
   })
 
-  const {
-    height
-  } = useWindowDimensions();
+  const {height} = useWindowDimensions();
   const navigation = useNavigation();
 
   const onSignInPressed = () => {
     db.transaction(tx => {
       tx.executeSql(`SELECT Password FROM Artker WHERE Username = '${username}'`,
-        null,
-        (txObj, resultSet) => {
-          let results = resultSet.rows._array
-          if (results.length == 0) {
-            console.log('No results')
-          } else {
-            let userObj = results[0]
-            let userPassword = userObj['Password']
-            if (userPassword == password) {
-              navigation.navigate('Home')
-            } else {
-              console.log('Wrong Password')
-            }
+      null,
+      (txObj, resultSet) => {
+        let results = resultSet.rows._array
+        if (results.length == 0) {
+          console.log('No results')
+        }
+        else{
+          let userObj = results[0]
+          let userPassword = userObj['Password']
+          if (userPassword == password) {
+            navigation.navigate('Home')
           }
-        },
-        (txObj, error) => console.log(error)
+          else {
+            console.log('Wrong Password')
+          }
+        }
+      },
+      (txObj, error) => console.log(error)
       )
     })
   }
@@ -75,81 +60,24 @@ export default function LoginScreen() {
     navigation.navigate('ForgotPassword')
   }
 
-  return ( <
-    ScrollView style = {
-      {
-        backgroundColor: colours.primary
-      }
-    }
-    showsVerticalScrollIndicator = {
-      false
-    } >
-    <
-    Screen style = {
-      styles.container
-    } >
-    <
-    Image source = {
-      Logo
-    }
-    style = {
-      [styles.logo, {
-        height: height * 0.3
-      }]
-    }
-    resizeMode = 'contain' / >
+  return (
+    <ScrollView style={{backgroundColor: colours.primary}} showsVerticalScrollIndicator={false}>
+      <Screen style={styles.container}>
+        <Image source={Logo} style={[styles.logo, {height: height * 0.3}]} resizeMode='contain'/>
 
-    <
-    CustomInput placeholder = 'Username'
-    value = {
-      username
-    }
-    setValue = {
-      setUsername
-    }
-    /> <
-    CustomInput placeholder = 'Password'
-    value = {
-      password
-    }
-    setValue = {
-      setPassword
-    }
-    secureTextEntry = {
-      true
-    }
-    />
+        <CustomInput placeholder='Username' value={username} setValue={setUsername}/>
+        <CustomInput placeholder='Password' value={password} setValue={setPassword} secureTextEntry={true}/>
 
-    <
-    CustomButton1 onPress = {
-      onSignInPressed
-    }
-    text = 'Sign In'
-    type = 'Primary' / >
-    <
-    CustomButton1 onPress = {
-      onForgotPasswordPressed
-    }
-    text = 'Forgot Password?'
-    type = 'Tertiary' / >
+        <CustomButton1 onPress={onSignInPressed} text='Sign In' type='Primary'/>
+        <CustomButton1 onPress={onForgotPasswordPressed} text='Forgot Password?' type='Tertiary'/>
 
-    <
-    View style = {
-      styles.bottomButtons
-    } >
-    <
-    SocialSignInButtons / >
+        <View style={styles.bottomButtons}>
+          <SocialSignInButtons/>
 
-    <
-    CustomButton1 onPress = {
-      onSignUpPressed
-    }
-    text = "Don't have an account? Create one"
-    type = 'Tertiary' / >
-    <
-    /View> <
-    /Screen> <
-    /ScrollView>
+          <CustomButton1 onPress={onSignUpPressed} text="Don't have an account? Create one" type='Tertiary'/>
+        </View>
+      </Screen>
+    </ScrollView>
   )
 }
 
