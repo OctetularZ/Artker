00684dce2201e85cca873cf5ed2b192da3cd1bd1
OctetularZ <taiabayomi15@gmail.com>
2022-12-20 12:53:53 +0000

    Added a few things to CreateProfileScreen such as: Function for button press and inputs

--- app/screens/CreateProfileScreen.js
+++ app/screens/CreateProfileScreen.js
@@ -3,29 +3,43 @@ import React, { useState, useEffect } from 'react'
 
 import Screen from '../components/Screen'
 import colours from '../config/colours'
+import CustomInput from '../components/CustomInput'
+import CustomButton1 from '../components/CustomButton1'
 import { ScrollView } from 'react-native-gesture-handler'
 import * as SQLite from 'expo-sqlite'
 
 
 export default function CreateProfileScreen({ route }) {
   const { usernamePassed } = route.params;
-  let username = JSON.stringify(usernamePassed)
+  let usernameDB = JSON.stringify(usernamePassed)
 
+  const [Name, setName] = useState('');
+  const [pfp, setPfp] = useState('');
+  const [phone, setPhone] = useState('');
+  const [expertise, setExpertise] = useState('');
+  
   const db = SQLite.openDatabase('Artker')
 
   useEffect(() => {
     db.transaction(tx => {
       tx.executeSql('CREATE TABLE IF NOT EXISTS Profile (id INTEGER PRIMARY KEY AUTOINCREMENT, Username TEXT, pfp TEXT)')
     });
-    db.transaction(tx => {
-      tx.executeSql(`INSERT INTO Profile (Username) VALUES ('${username}')`)
-    })
   })
+
+
+  const onCreateProfilePressed = () => {
+    db.transaction(tx => {
+      tx.executeSql(`INSERT INTO Profile (Username, ProfileUsername, Pfp, Phone, Rating, Expertise) VALUES ('${usernameDB}', '${Name}')`)
+    })
+  }
+
 
   return (
     <ScrollView style={{backgroundColor: colours.primary}} showsVerticalScrollIndicator={false}>
       <Screen style={styles.container}>
-        
+        <CustomInput placeholder='Name' value={Name} setValue={setName}/>
+
+        <CustomButton1 onPress={onCreateProfilePressed} text='Create Profile' type='Primary'/>
       </Screen>
     </ScrollView>
     // Navigate to the 'StarterScreen' when done designing
