79e2fbf4c40dc17ac5905a094bbf0becf7b1b070
OctetularZ <taiabayomi15@gmail.com>
2023-01-21 23:02:06 +0000

    Finished circle for pfp

--- app/components/PfpDisplay.js
+++ app/components/PfpDisplay.js
@@ -1,16 +1,26 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import colours from '../config/colours'
 
 export default function PfpDisplay() {
   return (
     <TouchableOpacity style={styles.pfp}>
-
+      
     </TouchableOpacity>
   )
 }
 
 const styles = StyleSheet.create({
   pfp: {
-    
+    height: 200,
+    width: 200,
+    backgroundColor: colours.secondaryBlack,
+    borderRadius: 100,
+    marginTop: 20,
+    marginBottom: 40,
+    shadowColor: 'black',
+    shadowOffset: {height: 0, width: 0},
+    shadowRadius: 10,
+    shadowOpacity: 0.2
   }
 })
