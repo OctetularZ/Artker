6a571f7c49232e41f16cae02368c05ce0963b7de
OctetularZ <taiabayomi15@gmail.com>
2023-01-21 23:07:51 +0000

    Finished front-end of pfp display

--- app/components/PfpDisplay.js
+++ app/components/PfpDisplay.js
@@ -2,10 +2,12 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import colours from '../config/colours'
 
+import { AntDesign } from '@expo/vector-icons'
+
 export default function PfpDisplay() {
   return (
     <TouchableOpacity style={styles.pfp}>
-      
+      <AntDesign name='plus' size={40} color='grey'/>
     </TouchableOpacity>
   )
 }
@@ -21,6 +23,8 @@ const styles = StyleSheet.create({
     shadowColor: 'black',
     shadowOffset: {height: 0, width: 0},
     shadowRadius: 10,
-    shadowOpacity: 0.2
+    shadowOpacity: 0.2,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 })
