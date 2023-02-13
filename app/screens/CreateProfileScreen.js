033b67a33371498be4664dc415ac43537925dc03
OctetularZ <taiabayomi15@gmail.com>
2023-02-13 11:29:04 +0000

    changed placeholder of expertises

--- app/screens/CreateProfileScreen.js
+++ app/screens/CreateProfileScreen.js
@@ -35,7 +35,7 @@ export default function CreateProfileScreen({ route }) {
   const [DOB, setDOB] = useState(new Date());
   let expertiseDisplay = ''
   let expertiseList = [] // Create flatlist for instruments and make searchable
-  const [skills, setSkills] = useState('Expertises');
+  const [skills, setSkills] = useState('Expertises - The first one selected will be your primary expertise and will be displayed on the short-hand of your profile');
   const [country, setCountry] = useState('Nationality');
   const [description, setDescription] = useState('');
   
