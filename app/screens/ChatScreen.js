b72e8a21205deaed20fec5f8c0fa1c17bc882ee0
OctetularZ <taiabayomi15@gmail.com>
2023-02-28 19:46:29 +0000

    discovered snapshot is required and is not supported by sql for firebase is required

--- app/screens/ChatScreen.js
+++ app/screens/ChatScreen.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useLayoutEffect, useState } from 'react'
+import React, { useEffect, useLayoutEffect, useState, useCallback } from 'react'
 
 import colours from '../config/colours'
 import { ScrollView } from 'react-native-gesture-handler'
@@ -7,6 +7,10 @@ import * as SQLite from 'expo-sqlite'
 import { useNavigation } from '@react-navigation/native'
 import { MaterialIcons, AntDesign, Feather } from '@expo/vector-icons'
 import { GiftedChat } from 'react-native-gifted-chat'
+import { db as fbDB } from '../../firebase'
+
+//Add navigation to replace to ensure users can't slide screen back
+// navigation.canGoback to check if user has a back button or not then you can replace screen as above
 
 export default function ChatScreen({ route }) {
   const navigation = useNavigation();
@@ -30,12 +34,6 @@ export default function ChatScreen({ route }) {
   const [receivername, setReceiverName] = useState(null);
   const [receiverPfp, setReceiverPfp] = useState(null);
   const [receiveremail, setReceiverEmail] = useState(null);
-
-  useEffect(() => {
-    db.transaction(tx => {
-      tx.executeSql('CREATE TABLE IF NOT EXISTS Chats (id INTEGER PRIMARY KEY, createdAt TEXT, text TEXT, user TEXT)')
-    });
-  }, []);
 
   useEffect(() => {
     db.transaction(tx => {
@@ -113,34 +111,6 @@ export default function ChatScreen({ route }) {
     });
   }, []);
 
-  const addMessageToDB = (id, createdAt, text, user) => {
-    db.transaction(tx => {
-      tx.executeSql(`INSERT INTO Chats (id, createdAt, text, user) VALUES ('${id}', '${createdAt}', '${text}', '${user}')`)
-    });
-  }
-
-  const listenToDB = () => {
-    db.transaction(tx => {
-      tx.executeSql(`SELECT * FROM Chats WHERE Username IN ('${username}', '${receiverUsername}') ORDER BY createdAt DESC`,
-      null,
-      (txObj , resultSet) => {
-        let results = resultSet.rows._array
-        console.log(results)
-        if (results.length === 0) {}
-        else {
-          let userInfo = results[0]
-          let pfp = userInfo['Pfp']
-          let name = userInfo['Name']
-
-          setName(name);
-          setPfp(pfp);
-        }
-      },
-      (txObj, error) => console.log(error)
-      )
-    });
-  }
-
   useEffect(() => {
     setMessages([
       {
@@ -160,9 +130,7 @@ export default function ChatScreen({ route }) {
     setMessages(previousMessages => GiftedChat.
     append(previousMessages, messages))
     const { _id, createdAt, text, user } = messages[0]
-    user = JSON.stringify(user)
-    addMessageToDB(_id, createdAt, text, user) // Adds msg to DB
-    // make function to run whenever the user sends a message
+    fbDB.collection('chats').add({_id, createdAt, text, user})
   }, [])
 
   return (
---
+++ firebase.js
@@ -0,0 +1,22 @@
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDymEDiGgQQuu_tiKK7oH6HkI3qtzq7q1w",
+  authDomain: "artker-8eda1.firebaseapp.com",
+  projectId: "artker-8eda1",
+  storageBucket: "artker-8eda1.appspot.com",
+  messagingSenderId: "804119564822",
+  appId: "1:804119564822:web:b5b25e941c42d4aba3db37"
+};
+
+let app;
+if (firebase.apps.length == 0) {
+  app = firebase.initializeApp(firebaseConfig);
+}
+else {
+  app = firebase.app()
+}
+
+const db = app.firestore();
+export {db};
