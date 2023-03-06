0f4e97cf9b9c671b5de1a05681a6f569d1e50da6
OctetularZ <taiabayomi15@gmail.com>
2023-03-06 20:18:30 +0000

    started ProfileScreen

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -3,11 +3,27 @@ import React, { useState, useEffect } from 'react'
 
 import colours from '../config/colours'
 import { ScrollView } from 'react-native-gesture-handler'
-import * as SQLite from 'expo-sqlite'
+import { db as fbDB } from '../../firebase'
+import { getAllData, delDocs, getData } from '../database/dbScripts'
+import { useNavigation } from '@react-navigation/native'
 
 
-export default function ProfileScreen() {
-  const db = SQLite.openDatabase('Artker')
+export default function ProfileScreen({ route }) {
+  const { usernamePassed } = route.params;
+  let username = JSON.stringify(usernamePassed)
+  username = username.replace(/\\/g, '')
+  username = username.replace(/"/g, '')
+
+  const [id, setID] = useState(null);
+  const [usernameDB, setUsernameDB] = useState(username);
+  const [name, setName] = useState(null);
+  const [Pfp, setPfp] = useState(null);
+  const [nationality, setNationality] = useState(null);
+  const [userExpertises, setUserExpertises] = useState(null);
+  const [DOB, setDOB] = useState(null);
+  const [description, setDescription] = useState(null);
+    
+  const navigation = useNavigation();
 
   return (
     <ScrollView style={{backgroundColor: colours.primary}} showsVerticalScrollIndicator={false}>
